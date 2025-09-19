470ea26ab7d6801e94d9d3c0f7fa57cea1c98bf0
Q Xu <xuq3196@outlook.com>
2025-09-19 23:28:21 +0800

    use rclone bisync

--- sync_script/sync.sh
+++ sync_script/sync.sh
@@ -3,7 +3,8 @@
 
 inotifywait -m -e close_write /home/xu/.local/share/fcitx5/rime/sync/lab_cachyos/user.yaml |
 while read -r filename event ; do
-    rclone sync /home/xu/.local/share/fcitx5/rime/sync onedrive:/Apps/Rime -v
+    # must run `rclone bisync /home/xu/.local/share/fcitx5/rime/sync onedrive:/Apps/Rime --resync` first
+    rclone bisync /home/xu/.local/share/fcitx5/rime/sync onedrive:/Apps/Rime -v
     echo "$(date): Synced due to $event on $filename"
     sleep 1
 done
