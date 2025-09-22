846d47c299db49e3777df6d4f2ff3cce37b5019a
Q Xu <xuq3196@outlook.com>
2025-09-22 17:31:01 +0800

    update sync script

--- sync_script/sync.sh
+++ sync_script/sync.sh
@@ -1,10 +1,42 @@
 # !/bin/bash
 # 使用inotifywait监控文件变化，并触发rclone同步
 
-inotifywait -m -e close_write /home/xu/.local/share/fcitx5/rime/sync/lab_cachyos/user.yaml |
-while read -r filename event ; do
-    # must run `rclone bisync /home/xu/.local/share/fcitx5/rime/sync onedrive:/Apps/Rime --resync` first
-    rclone bisync /home/xu/.local/share/fcitx5/rime/sync onedrive:/Apps/Rime -v
-    echo "$(date): Synced due to $event on $filename"
-    sleep 1
+FILE_TO_WATCH="/home/xu/.local/share/fcitx5/rime/sync/lab_cachyos/user.yaml"
+
+RCLONE_SOURCE="/home/xu/.local/share/fcitx5/rime/sync"
+RCLONE_DEST="onedrive:/Apps/Rime"
+
+
+if [ ! -f "$FILE_TO_WATCH" ]; then
+    echo "error: file not found: $FILE_TO_WATCH"
+    exit 1
+fi
+
+LAST_HASH=$(md5sum "$FILE_TO_WATCH" | awk '{print $1}')
+
+echo "script start：start watching $FILE_TO_WATCH"
+echo "init hash: $LAST_HASH"
+echo "-----------------------------------------------------"
+
+inotifywait -m -e close_write "$FILE_TO_WATCH" |
+while read -r filename event; do
+    CURRENT_HASH=$(md5sum "$FILE_TO_WATCH" | awk '{print $1}')
+
+    if [ "$CURRENT_HASH" != "$LAST_HASH" ]; then
+        echo "$(date): file change detected!"
+        echo "  old hash: $LAST_HASH"
+        echo "  new hash: $CURRENT_HASH"
+        echo "  >> syncing..."
+
+        # must run `rclone bisync /home/xu/.local/share/fcitx5/rime/sync onedrive:/Apps/Rime --resync` first
+        rclone bisync "$RCLONE_SOURCE" "$RCLONE_DEST" -v
+        
+        echo "  >> sync done."
+        
+        LAST_HASH=$CURRENT_HASH
+        echo "-----------------------------------------------------"
+    else
+        echo "$(date): file change detected, but hash is the same. No sync needed."
+        echo "-----------------------------------------------------"
+    fi
 done
